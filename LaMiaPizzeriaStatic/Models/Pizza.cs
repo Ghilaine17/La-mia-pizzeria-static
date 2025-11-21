4ca8ca66649570930f567205718009af2743c930
Ghilaine17 <cardinalealessia3@gmail.com>
2025-11-21 09:52:01 +0100

    first commit

---
+++ LaMiaPizzeriaStatic/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaMiaPizzeriaStatic.Models
+{
+    public class Pizza
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Description { get; set; }
+        public string UrlPhoto { get; set; }
+        [Required] public double Price { get; set; } 
+
+        public Pizza(string name, string description, string urlPhoto, double price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.UrlPhoto = urlPhoto;
+            this.Price = price;
+        }
+    }
+
+}
---
+++ LaMiaPizzeriaStatic/Models/PizzeriaDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeriaStatic.Models
+{
+    public class PizzeriaDb : DbContext
+    {
+        public DbSet<Pizza> pizze {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzeriaDb;" +
+             "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
